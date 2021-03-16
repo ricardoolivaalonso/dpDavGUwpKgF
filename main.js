3ed8a60f9f34df40831eddbdac25ccc931c3c4e6
Ricardo Oliva Alonso <46902833+ricardoolivaalonso@users.noreply.github.com>
2021-03-16 15:23:31 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -17,10 +17,41 @@ let ca = document.querySelectorAll('.circle-a')
 let cb = document.querySelectorAll('.circle-b')
 let cc = document.querySelectorAll('.circle-c')
 
-
 let spe = document.querySelector('#speaker-t');
 
+let smallS = document.querySelector('#small-s')
+let smallSpeakers = document.querySelectorAll('.small-speakers')
 
+let handleE = document.querySelector('#handle-e')
+let handle = document.querySelectorAll('.handle-e')
+
+let antennaL = document.querySelector('#antenna-l')
+let antennaLE = document.querySelectorAll('.antenna-l')
+
+let antennaR = document.querySelector('#antenna-r')
+let antennaRE = document.querySelectorAll('.antenna-r')
+
+/*******************/
+let antennaRFunc = () => {
+    console.log(antennaRE)
+
+    if(antennaR.checked) antennaRE.forEach( f => f.classList.add('s'))
+    else antennaRE.forEach( f => f.classList.remove('s'))
+}
+let antennaLFunc = () => {
+    console.log(antennaLE)
+
+    if(antennaL.checked) antennaLE.forEach( f => f.classList.add('s'))
+    else antennaLE.forEach( f => f.classList.remove('s'))
+}
+let handleFunc = () => {
+    if(handleE.checked) handle.forEach( f => f.classList.add('s'))
+    else handle.forEach( f => f.classList.remove('s'))
+}
+let smallSpeakerFunc = () => {
+    if(smallS.checked) smallSpeakers.forEach( f => f.classList.add('s'))
+    else smallSpeakers.forEach( f => f.classList.remove('s'))
+}
 let speFunc = () => {
     let speaker = spe.value
     if(speaker == 'square-speaker'){
@@ -31,14 +62,7 @@ let speFunc = () => {
         document.querySelectorAll('.square-speaker').forEach( f => f.classList.add('s'))
         document.querySelectorAll('.circle-speaker').forEach( f => f.classList.remove('s'))
     }   
-  
-
-
 }
-
-spe.addEventListener('change', speFunc)
-
-/*******************/
 let playAudio = () => {
     a.loop = true;
 
@@ -47,7 +71,6 @@ let playAudio = () => {
         a.pause();
         a.currentTime = 0;
     }
- 
     // Square speaker
     sfa && sfa.forEach( f => f.classList.toggle('sfa') )
     sta && sta.forEach( f => f.classList.toggle('sta') )
@@ -61,11 +84,14 @@ let playAudio = () => {
     // Circle speaker
     visualizer && visualizer.forEach( f => f.classList.toggle('opt') )
     boombox.classList.toggle('boombox-a')
-
     // Icon speaker
     son.classList.toggle('s')
     soff.classList.toggle('s')
 }
-
 /*******************/
 b.addEventListener('click', playAudio)
+spe.addEventListener('change', speFunc)
+smallS.addEventListener('change', smallSpeakerFunc)
+handleE.addEventListener('change', handleFunc)
+antennaL.addEventListener('change', antennaLFunc)
+antennaR.addEventListener('change', antennaRFunc)
